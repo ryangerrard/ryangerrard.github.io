17eb5936caec9aab2b272d6685e118a15aa4a970
i819635 <ryan.gerrard@sap.com>
2020-05-17 20:17:47 -0700

    css testing

--- sac-custom-widgets/autocomplete/webcomponent.js
+++ sac-custom-widgets/autocomplete/webcomponent.js
@@ -207,7 +207,7 @@
                 outline-offset: -3px;
             }
 
-            .decisionIncInputField:focus +.decisionIncComponentWidgetPanel{
+            .decisionIncInputField:focus{
                 overflow-x: hidden;
                 overflow-y: scroll;
             }
