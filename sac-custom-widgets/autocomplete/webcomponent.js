fadbae7cba9259263472c67ff48e152dd9f971d8
i819635 <ryan.gerrard@sap.com>
2020-05-19 15:20:12 -0700

    css updates

--- sac-custom-widgets/autocomplete/webcomponent.js
+++ sac-custom-widgets/autocomplete/webcomponent.js
@@ -5,7 +5,7 @@
     <div id="di-autocomplete-main"
     class="decisionIncComponentWidgetPanel">
         <input id="di-autocomplete-widget" class="decisionIncInputField " value="" key="" type="text"
-            placeholder="Enter Text..."
+            placeholder="..."
             style="font-size: 14px; border-color: rgb(191, 191, 191); 
             background-color: rgb(255, 255, 255);">
 
